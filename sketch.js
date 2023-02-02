df2ea721e432ae74e9d40ccdccc42e77baa0bc0c
anandupadhayay <124306724+anandupadhayay@users.noreply.github.com>
2023-02-02 15:11:28 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,154 @@
+let img;
+let img_1;
+
+function preload() {
+  //img = loadImage('bison.png');
+  img = loadImage('img_1.png');
+}
+
+// V A R I A B L E S
+
+var bubble = [];
+var mySize;
+var noiseMax;  
+
+let phase = 0;
+let zoff = 0;
+let angle = 30;
+
+    // S E T U P 
+
+function setup() {
+  createCanvas(800,500);
+  //background(0);
+  for (var i = 0; i<20; i++) {
+    bubble[i] = new Bubble();
+
+  }
+}
+
+    // D R A W
+
+function draw() {
+  
+  background(220);
+  sandstorm.display();
+
+  for (var i = 0; i< bubble.length; i++) {
+  grid.display();
+    
+  bubble[i].display();
+  bubble[i].move();
+    
+  // I M A G E S. -- -- --  IMG AND IMG_1
+    
+  image(img, random(width), random(height),200, 100);
+    
+  //image(img_1,200,300,200,100);
+
+    
+    
+  push();
+  //frameRate(100);
+  fill(255,0,255); 
+  stroke(255,0,255);
+  strokeWeight(4)
+    
+  //  d r a w i n g " + " P O I N T
+    
+  rect(mouseX+20,mouseY,20,60);
+  rect(mouseX,mouseY+20,60,20);
+      //rect(mouseX+80,mouseY,60,20);
+      //rect(mouseX,mouseY+80,20,60);
+
+  pop();
+
+  }
+}
+    
+    // G R I D
+
+var grid = {
+  
+  v : 15,
+  f : 11,
+  
+  display : function () {
+    
+    for (var x = 0; x < width; x += width / this.v) {
+		for (var y = 0; y < height; y += height / this.f) {
+			stroke(0);
+			strokeWeight(1);
+			line(x, 0, x, height);
+			line(0, y, width, y);
+}
+  }
+  }
+}
+
+      // F L O A T I N G  C I R C L E S
+
+function Bubble() {
+  
+  this.x = random(0, width);
+  this.y = random(0,height);
+  
+  this.display = function(){
+  //stroke(2);
+  //strokeWeight(2);
+  fill(255,255,255);
+  ellipse(this.x, this.y, 50,50);
+  }
+  
+  this.move = function() {
+  
+  this.x = this.x + random(-1,1);
+  this.y = this.y + random(-1,1);   
+  }
+}
+
+  // S A N D S T O R M
+
+var sandstorm = {
+  
+  display : function() {
+    
+  mySize = min(width, height);
+  noiseMax = random(2000);
+   
+  // dotted wave
+    
+  push();
+  translate(width / 2, height / 4);   // to shake the epicenter of sandstorm
+  noFill();
+  for (var i = 0; i < 100; i += 2) {
+
+    beginShape();
+    for (var a = 0; a < TWO_PI; a += 0.01) {
+      let xoff = map(cos(a + phase), -1, 1, 0, noiseMax);
+      let yoff = map(sin(a + phase / 2), -1, 1, 0, noiseMax);
+      let r = map(noise(xoff, yoff, zoff), 0, 1, 0, mySize / 1.5 - i * 3.0);
+      let r1 = map(noise(xoff, yoff, zoff), 0, 1, 0, mySize / 1.5 - i * 3.0);
+      let x = r * cos(a);
+      let y = r1 * sin(a);
+
+      //strokeWeight(random(3));
+      //point(x+random(5,-5), y+random(150,-5));
+      //point(x*random(0,mouseX),y*random(mouseY,0));
+      point(x+mouseX/2,y+mouseY/3);
+
+
+    }
+    //endShape(CLOSE);
+    phase += 4;
+    }
+  pop();
+  }
+}
+
+    //S A V E - Y O U R - D R A W I N G 
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (keyCode == DELETE || keyCode == BACKSPACE) clear();
+}
